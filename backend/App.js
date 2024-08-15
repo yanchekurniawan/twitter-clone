71a41598117299b1fc42bfa2033f0560d316cdee
Yanche Kurniawan Mangalik <yanche.k.m@gmail.com>
2024-08-15 17:53:24 +0800

    First Production Preparation

--- backend/App.js
+++ backend/App.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -17,6 +18,8 @@ cloudinary.config({
 });
 
 const app = express();
+const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({ extended: true, limit: "5mb" }));
 app.use(cors());
@@ -27,7 +30,13 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV.trim() == "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
