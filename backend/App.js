ac12ba0893257ca7159fe2b50f74e1f5a3064de8
Yanche Kurniawan Mangalik <yanche.k.m@gmail.com>
2024-08-15 19:07:46 +0800

    First Deployment Preparation (2)

--- backend/App.js
+++ backend/App.js
@@ -25,6 +25,9 @@ app.use(express.json({ extended: true, limit: "5mb" }));
 app.use(cors());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.json("Hello");
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
