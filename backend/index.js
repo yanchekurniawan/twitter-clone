1870c2f878a2e0fc1f592ea3f252b2ec3b20ee8c
Yanche Kurniawan Mangalik <yanche.k.m@gmail.com>
2024-08-16 09:42:14 +0800

    Fixing Production 3

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
-/* import path from "path"; */
+import path from "path";
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import notificationRoutes from "./routes/notificationRoutes.js";
-import testRoutes from "./routes/testRoutes.js";
 import dotenv from "dotenv";
 import connectToMongoDB from "./db/databaseConfig.js";
 import cookieParser from "cookie-parser";
@@ -20,28 +19,27 @@ cloudinary.config({
 
 const app = express();
 const port = process.env.PORT || 5000;
-/* const __dirname = path.resolve(); */
+const __dirname = path.resolve();
 
 app.use(express.json({ extended: true, limit: "5mb" }));
 app.use(cors());
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.json("Hello Guys");
+  res.json("Hello");
 });
-app.use("/api/testing", testRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-/* if (process.env.NODE_ENV.trim() == "production") {
+if (process.env.NODE_ENV.trim() == "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
-} */
+}
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
